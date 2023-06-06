4dcca30e51e8c8bbdcee5839f534fae9a577f3ca
bforbiggy <AndyHeCS@protonmail.com>
2023-06-06 13:22:21 -0400

    ye

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,17 +1,30 @@
 <script>
 	import "../lib/styles.scss";
+	import logo from "../lib/images/logo.png";
 </script>
 
-<div class="footer">THIS WILL BE ON EVERY PAGE</div>
+<div class="footer">
+	<img alt="" src={logo} />
+
+	<a href="contact/">Contact</a>
+</div>
 <slot />
 
 <style lang="scss">
 	@use "../lib/theme.scss" as theme;
 
 	.footer {
-		width: 100vw;
 		position: absolute;
 		top: 0px;
+
+		width: 100vw;
 		background-color: theme.$sub;
+		width: min-content;
+		display: flex;
+
+		img {
+			max-height: 10rem;
+			width: auto;
+		}
 	}
 </style>
